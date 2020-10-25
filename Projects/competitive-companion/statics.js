33b90091e3dd19b1d75f8e217474b338612f9a5a
DhruvGheewala <dhruvgheewala.4g@gmail.com>
2020-10-26 00:49:12 +0530

    Update statics.js

--- Projects/competitive-companion/statics.js
+++ Projects/competitive-companion/statics.js
@@ -1,7 +1,58 @@
-dir = 'D:/Dhruv/Codes/C++/';
-separator = '\n$###%###%#############%###%###$\n';
+// NPM Packages
+const { debug } = require('console');
+const fs = require('fs');
 
-let data = { dir, separator };
-module.exports = data;
+// Constants
+const dir = 'D:/Dhruv/Codes/C++/';
+const separator = '\n$###%###%#############%###%###$\n';
 
-module.exports = { dir, separator };
+// Functions
+function updateFile(path, data, fxn = () => {}) {
+    fs.writeFile(path, data, (err) => {
+        if (err)
+            throw err;
+        fxn();
+    });
+}
+
+function readFile(path, fxn) {
+    fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+        if (err)
+            throw err;
+        let code = '';
+        if (data)
+            code += data;
+        fxn(code);
+    });
+}
+
+function removeTitle(fxn = () => {}) {
+    readFile(dir + '1.cpp', (code) => {
+        if (code.startsWith('// ')) {
+            let new_code = '';
+            let fir = true;
+
+            for (let i = 0; i < code.length; i++) {
+                var char = code[i];
+                if (fir && char == '\n') {
+                    fir = false;
+                    continue;
+                }
+                if (!fir)
+                    new_code += char;
+            }
+            updateFile(dir + '1.cpp', new_code, fxn);
+        } else {
+            fxn();
+        }
+    });
+}
+
+// Exports
+module.exports = {
+    dir,
+    separator,
+    updateFile,
+    readFile,
+    removeTitle
+};
